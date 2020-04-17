6eb338f0b490c973004f569b0b82fc219bfd9e3d
Kaspersen <vi1511@vetinst.no>
2020-04-17 08:38:29 +0200

    added func_paste function

---
+++ R/func_paste.R
@@ -0,0 +1,12 @@
+#' Paste together unique values
+#'
+#' Paste together unique values in a column, and collapse with specified symbol.
+#'
+#' @param x The column holding the vaues
+#' @param collapse Delimiter between the unique values, default ","
+#'
+#' @author Håkon Kaspersen, \email{hakon.kaspersen@@vetinst.no}
+#'
+#' @export
+#'
+func_paste <- function(x, collapse = ",") paste(unique(x[!is.na(x)]), collapse = collapse)
